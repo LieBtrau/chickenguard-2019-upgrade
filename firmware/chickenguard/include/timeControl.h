1bb994a1d4bf2aff1e522d8660716b1264835d6d
Christoph Tack <prog.send@gmail.com>
2023-07-01 13:38:36 +0200

    Implement alarm + taking care of time zones

--- firmware/chickenguard/include/timeControl.h
+++ firmware/chickenguard/include/timeControl.h
@@ -2,21 +2,33 @@
 
 #include <Arduino.h>
 #include "DS1337.h"
-
+#include "AsyncDelay.h"
 
 class TimeControl
 {
 public:
-    TimeControl(int8_t (*readBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, uint8_t *data), 
-        bool (*writeBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, const uint8_t *data));
+    TimeControl(int8_t (*readBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, uint8_t *data),
+                bool (*writeBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, const uint8_t *data),
+                void (*alarm1Triggered)(void),
+                void (*alarm2Triggered)(void));
     ~TimeControl();
 
     bool init(String timeZone);
     bool hasValidTime();
     bool updateMcuTime(long utc, const String timeZone);
-
+    bool setOpenAlarmSunrise(double latitude, double longitude);
+    bool setOpenAlarmFixTime(uint8_t hour, uint8_t minute);
+    bool setCloseAlarmSunset(double latitude, double longitude);
+    bool setCloseAlarmFixTime(uint8_t hour, uint8_t minute);
+    bool disableAlarms();
+    bool run();
 private:
     bool setTimeZone(String timeZone);
+    void doubleToHrMin(double time, uint8_t *hr, uint8_t *min);
+    struct tm* toUtc(uint8_t hr, uint8_t min);
     DS1337 _rtc;
-    bool _timeZoneSet=false;
+    bool _timeZoneSet = false;
+    void (*_alarm1Triggered)(void);
+    void (*_alarm2Triggered)(void);
+    AsyncDelay _alarmPollingDelay;
 };
--- firmware/chickenguard/src/timeControl.cpp
+++ firmware/chickenguard/src/timeControl.cpp
@@ -1,8 +1,23 @@
+/**
+ * @file timeControl.cpp
+ * @author Christoph Tack
+ * @brief
+ * @version 0.1
+ * @date 2023-06-25
+ * @details
+ *   When using the sunrise and sunset functions, the new sunset alarm will be set just after sunrise.  The new sunrise alarm will be set
+ * just after sunset.  This is to prevent the alarm from triggering immediately after setting it (which would be the case for sunrise times
+ * after summer solstice and sunset times before summer solstice).
+ * @copyright Copyright (c) 2023
+ *
+ */
 #include "timeControl.h"
 #include "i2c_hal.h"
 #include <SolarCalculator.h>
 
 static const char *TAG = "timeControl";
+
+static time_t timegm(struct tm *t);
 
 typedef struct
 {
@@ -13,13 +28,15 @@ typedef struct
 static const timeZone_t timeZones[1] =
     {
         {"Europe/Brussels", "CET-1CEST,M3.5.0,M10.5.0/3"} // source : last line of /usr/share/zoneinfo/Europe/Brussels)
-        // Add more timezones here
+                                                          // Add more timezones here
 };
 
-TimeControl::TimeControl(
-    int8_t (*readBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, uint8_t *data),
-    bool (*writeBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, const uint8_t *data)) : _rtc(readBytes, writeBytes),
-                                                                                               _timeZoneSet(false)
+TimeControl::TimeControl(int8_t (*readBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, uint8_t *data),
+                         bool (*writeBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, const uint8_t *data),
+                         void (*alarm1Triggered)(void),
+                         void (*alarm2Triggered)(void)) : _rtc(readBytes, writeBytes), _timeZoneSet(false),
+                                                          _alarm1Triggered(alarm1Triggered), _alarm2Triggered(alarm2Triggered),
+                                                          _alarmPollingDelay(1000, AsyncDelay::MILLIS)
 {
 }
 
@@ -39,15 +56,14 @@ bool TimeControl::init(String timeZone)
     assert(detectI2cDevice(_rtc.getI2cAddress()));
     if (_rtc.isTimeValid())
     {
-        time_t now;
-        // get current time from RTC
-        assert(_rtc.getTime(&now));
+        struct tm timeinfo;
+        assert(_rtc.getTime(&timeinfo));
+        time_t now = timegm(&timeinfo);
         // set current time to MCU
         timeval epoch = {now, 0};
         settimeofday((const timeval *)&epoch, 0);
 
         // print UTC-time using strftime() instead of the obsolete asctime()
-        struct tm timeinfo;
         gmtime_r(&now, &timeinfo);
         char strftime_buf[64];
         strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
@@ -63,7 +79,22 @@ bool TimeControl::init(String timeZone)
             strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
             ESP_LOGI(TAG, "The current local date/time is: %s", strftime_buf);
         }
+
+        // Did an alarm cause an MCU wakeup?
+        if (_rtc.isAlarmTriggered(DS1337::AlarmType::Alarm1))
+        {
+            ESP_LOGI(TAG, "Alarm 1 triggered");
+            _rtc.acknowledgeAlarm(DS1337::AlarmType::Alarm1);
+            _alarm1Triggered();
+        }
+        if (_rtc.isAlarmTriggered(DS1337::AlarmType::Alarm2))
+        {
+            ESP_LOGI(TAG, "Alarm 2 triggered");
+            _rtc.acknowledgeAlarm(DS1337::AlarmType::Alarm2);
+            _alarm2Triggered();
+        }
     }
+    _alarmPollingDelay.restart();
 
     return _rtc.enableSquareWave(false); // disable square wave output to save power
 }
@@ -80,7 +111,8 @@ bool TimeControl::updateMcuTime(long utc, const String timeZone)
     settimeofday((const timeval *)&epoch, 0);
     ESP_LOGI(TAG, "TimeZone : %s", timeZone.c_str());
 
-    if (!_rtc.setTime(utc))
+    tm *timeinfo = gmtime(&utc);
+    if (!_rtc.setTime(timeinfo))
     {
         ESP_LOGE(TAG, "Could not set RTC time");
         return false;
@@ -88,6 +120,13 @@ bool TimeControl::updateMcuTime(long utc, const String timeZone)
     return setTimeZone(timeZone);
 }
 
+/**
+ * @brief Set the time zone
+ *
+ * @param timeZone
+ * @return true when time zone is set
+ * @return false when time zone is not in the list of time zones
+ */
 bool TimeControl::setTimeZone(String timeZone)
 {
     for (int i = 0; i < sizeof(timeZones) / sizeof(timeZone_t); i++)
@@ -102,17 +141,158 @@ bool TimeControl::setTimeZone(String timeZone)
     }
     return _timeZoneSet;
 }
-// bool TimeControl::getSunrise(uint8_t *hour, uint8_t *minute, float latitude, float longitude)
-// {
-//     if (!_timeIsSet)
-//     {
-//         ESP_LOGE(TAG, "Time is not set");
-//         return false;
-//     }
-//     time_t utc;
-//     time(&utc);
-//     double transit, sunrise, sunset;
-//     calcSunriseSunset(utc, latitude, longitude, transit, sunrise, sunset);
-//     ESP_LOGI(TAG, "Sunrise: %f\tSunset: %f\r\n", sunrise, sunset);
-//     return true;
-// }
+
+bool TimeControl::setOpenAlarmSunrise(double latitude, double longitude)
+{
+    if (!hasValidTime())
+    {
+        ESP_LOGE(TAG, "Time is not set");
+        return false;
+    }
+    time_t utc;
+    time(&utc);
+    double transit, sunrise, sunset;
+    calcSunriseSunset(utc, latitude, longitude, transit, sunrise, sunset);
+    uint8_t hour, minute;
+    doubleToHrMin(sunrise, &hour, &minute);
+    ESP_LOGI(TAG, "Next Sunrise alarm (local time): %02d:%02d", hour, minute);
+    return _rtc.setDailyAlarm(DS1337::AlarmType::Alarm1, toUtc(hour, minute));
+}
+
+bool TimeControl::setCloseAlarmSunset(double latitude, double longitude)
+{
+    if (!hasValidTime())
+    {
+        ESP_LOGE(TAG, "Time is not set");
+        return false;
+    }
+    time_t utc;
+    time(&utc);
+    double transit, sunrise, sunset;
+    calcSunriseSunset(utc, latitude, longitude, transit, sunrise, sunset);
+    uint8_t hour, minute;
+    doubleToHrMin(sunset, &hour, &minute);
+    ESP_LOGI(TAG, "Next Sunset alarm (local time): %02d:%02d", hour, minute);
+    return _rtc.setDailyAlarm(DS1337::AlarmType::Alarm2, toUtc(hour, minute));
+}
+
+bool TimeControl::setOpenAlarmFixTime(uint8_t hour, uint8_t minute)
+{
+    if (!hasValidTime())
+    {
+        ESP_LOGE(TAG, "Time is not set");
+        return false;
+    }
+    ESP_LOGI(TAG, "Next fix open alarm (local time): %02d:%02d", hour, minute);
+    return _rtc.setDailyAlarm(DS1337::AlarmType::Alarm1, toUtc(hour, minute));
+}
+
+bool TimeControl::setCloseAlarmFixTime(uint8_t hour, uint8_t minute)
+{
+    if (!hasValidTime())
+    {
+        ESP_LOGE(TAG, "Time is not set");
+        return false;
+    }
+    ESP_LOGI(TAG, "Next fix close alarm (local time): %02d:%02d", hour, minute);
+    return _rtc.setDailyAlarm(DS1337::AlarmType::Alarm2, toUtc(hour, minute));
+}
+
+void TimeControl::doubleToHrMin(double time, uint8_t *hour, uint8_t *minute)
+{
+    int m = int(round(time * 60));
+    *hour = (m / 60) % 24;
+    *minute = m % 60;
+}
+
+/**
+ * @brief Run the time control
+ * @note This function should be called in the main loop.  It's used for debugging purposes only.
+ * During debugging, the MCU will be powered, so the incoming alarm will not wake up the MCU.  To simulate the behavior of waking up, this
+ * function will poll the RTC for an alarm trigger.
+ *
+ * @return true alarm triggered
+ * @return false no alarm triggered
+ */
+bool TimeControl::run()
+{
+    if (!_alarmPollingDelay.isExpired())
+    {
+        return false;
+    }
+    _alarmPollingDelay.restart();
+    if (_rtc.isAlarmTriggered(DS1337::AlarmType::Alarm1))
+    {
+        _rtc.acknowledgeAlarm(DS1337::AlarmType::Alarm1);
+        _alarm1Triggered();
+        return true;
+    }
+    if (_rtc.isAlarmTriggered(DS1337::AlarmType::Alarm2))
+    {
+        _rtc.acknowledgeAlarm(DS1337::AlarmType::Alarm2);
+        _alarm2Triggered();
+        return true;
+    }
+    return false;
+}
+
+bool TimeControl::disableAlarms()
+{
+    return _rtc.disableAlarm(DS1337::AlarmType::Alarm1) && _rtc.disableAlarm(DS1337::AlarmType::Alarm2);
+}
+
+/**
+ * @brief Convert hour and minute to UTC
+ * Useful for setting the alarm time, because the RTC is in UTC
+ * @param hour
+ * @param minute
+ * @return struct tm*
+ */
+struct tm *TimeControl::toUtc(uint8_t hour, uint8_t minute)
+{
+    time_t dummyTime;
+    time(&dummyTime);
+    struct tm *timeinfo = localtime(&dummyTime);
+    timeinfo->tm_hour = hour;
+    timeinfo->tm_min = minute;
+    timeinfo->tm_sec = 0;
+    time_t utc = mktime(timeinfo);
+    gmtime_r(&utc, timeinfo);
+    return timeinfo;
+}
+
+/**
+ * @brief Get the Time object as UTC
+ * https://stackoverflow.com/questions/530519/stdmktime-and-timezone-info#5157134
+ * @param t
+ * @return time_t
+ */
+time_t timegm(struct tm *t)
+{
+    long year;
+    time_t result;
+    const int MONTHSPERYEAR = 12; /* months per calendar year */
+    static const int cumdays[MONTHSPERYEAR] =
+        {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+    /*@ +matchanyintegral @*/
+    year = 1900 + t->tm_year + t->tm_mon / MONTHSPERYEAR;
+    result = (year - 1970) * 365 + cumdays[t->tm_mon % MONTHSPERYEAR];
+    result += (year - 1968) / 4;
+    result -= (year - 1900) / 100;
+    result += (year - 1600) / 400;
+    if ((year % 4) == 0 && ((year % 100) != 0 || (year % 400) == 0) &&
+        (t->tm_mon % MONTHSPERYEAR) < 2)
+        result--;
+    result += t->tm_mday - 1;
+    result *= 24;
+    result += t->tm_hour;
+    result *= 60;
+    result += t->tm_min;
+    result *= 60;
+    result += t->tm_sec;
+    if (t->tm_isdst == 1)
+        result -= 3600;
+    /*@ -matchanyintegral @*/
+    return (result);
+}
