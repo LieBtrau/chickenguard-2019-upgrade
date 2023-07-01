96a3dc18e85b1557a9c7761fd036f0f761bf59bd
Christoph Tack <prog.send@gmail.com>
2023-07-01 13:37:11 +0200

    Convert to class

--- firmware/chickenguard/include/Webservice.h
+++ firmware/chickenguard/include/Webservice.h
@@ -1,7 +1,28 @@
 #pragma once
 
 #include <Arduino.h>
+#include <DNSServer.h>
+#include <ESPAsyncWebServer.h>
+#include "NonVolatileStorage.h"
 
-void setupWebserver();
-void loopWebserver();
-void notifyClients(String key, String status);
+class Webservice
+{
+public:
+    Webservice(NonVolatileStorage* nonVolatileStorage, void (*updateTime)(long utc, const String timezone), void (*cbDataReceived)(void));
+    ~Webservice();
+    void setup();
+    void loop();
+    void notifyClients(String key, String status);
+    void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
+
+private:
+    const IPAddress localIP;    // the IP address the web server, Samsung requires the IP to be in public space
+    const IPAddress subnetMask; // no need to change: https://avinetworks.com/glossary/subnet-mask/
+    DNSServer dnsServer;
+    AsyncWebServer server; // HTTP port 80
+    AsyncWebSocket ws;
+    bool isInitialized = false;
+    NonVolatileStorage* _nonVolatileStorage;
+    void (*_cbDataReceived)(void) = nullptr;
+    void (*_updateTime)(long utc, const String timezone) = nullptr;
+};
--- firmware/chickenguard/src/Webservice.cpp
+++ firmware/chickenguard/src/Webservice.cpp
@@ -1,29 +1,66 @@
 #include "Webservice.h"
 #include <SPIFFS.h>
-#include <ESPAsyncWebServer.h>
+
 #include <ArduinoJson.h>
 #include "wifi_credentials.h"
-#include <DNSServer.h>
-#include "timeControl.h"
-#include "NonVolatileStorage.h"
 
 static const char *TAG = "Webservice";
+static Webservice *_instance = nullptr;
 
-void onRootRequest(AsyncWebServerRequest *request);
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+static String processor(const String &var)
+{
+    return String(var == "STATE" ? "on" : "off");
+}
 
-const IPAddress localIP(4, 3, 2, 1);          // the IP address the web server, Samsung requires the IP to be in public space
-const IPAddress subnetMask(255, 255, 255, 0); // no need to change: https://avinetworks.com/glossary/subnet-mask/
-const String localIPURL = "http://4.3.2.1";   // a string version of the local IP with http, used for redirecting clients to your webpage
+static void onRootRequest(AsyncWebServerRequest *request)
+{
+    AsyncWebServerResponse *response = request->beginResponse(SPIFFS, "/index.html", "text/html", false, processor);
+    response->addHeader("Cache-Control", "public,max-age=0"); //
+    request->send(response);
+}
 
-DNSServer dnsServer;
-AsyncWebServer server(80); // HTTP port 80
-AsyncWebSocket ws("/ws");
-extern TimeControl timeControl;
-extern NonVolatileStorage config;
-static bool isInitialized = false;
+static void onEvent(AsyncWebSocket *server,
+                    AsyncWebSocketClient *client,
+                    AwsEventType type,
+                    void *arg,
+                    uint8_t *data,
+                    size_t len)
+{
 
-void setupWebserver()
+    switch (type)
+    {
+    case WS_EVT_CONNECT:
+        ESP_LOGI(TAG, "WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        break;
+    case WS_EVT_DISCONNECT:
+        ESP_LOGI(TAG, "WebSocket client #%u disconnected\n", client->id());
+        break;
+    case WS_EVT_DATA:
+        _instance->handleWebSocketMessage(arg, data, len);
+        break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+        break;
+    }
+}
+
+Webservice::Webservice(NonVolatileStorage *nonVolatileStorage, void (*updateTime)(long utc, const String timezone),
+                       void (*cbDataReceived)(void)) : localIP(4, 3, 2, 1),
+                                                       subnetMask(255, 255, 255, 0),
+                                                       server(80),
+                                                       ws("/ws"),
+                                                       _nonVolatileStorage(nonVolatileStorage),
+                                                       _updateTime(updateTime),
+                                                       _cbDataReceived(cbDataReceived)
+{
+    _instance = this;
+}
+
+Webservice::~Webservice()
+{
+}
+
+void Webservice::setup()
 {
     if (!SPIFFS.begin())
     {
@@ -57,7 +94,7 @@ void setupWebserver()
 
     // the catch all
     server.onNotFound([](AsyncWebServerRequest *request)
-                      { request->redirect(localIPURL); });
+                      { request->redirect("http://4.3.2.1"); }); //// a string version of the local IP with http, used for redirecting clients to your webpage
 
     server.on("/", HTTP_ANY, onRootRequest);
     server.serveStatic("/", SPIFFS, "/");
@@ -65,7 +102,7 @@ void setupWebserver()
     isInitialized = true;
 }
 
-void loopWebserver()
+void Webservice::loop()
 {
     if (!isInitialized)
     {
@@ -75,19 +112,7 @@ void loopWebserver()
     ws.cleanupClients();
 }
 
-String processor(const String &var)
-{
-    return String(var == "STATE" ? "on" : "off");
-}
-
-void onRootRequest(AsyncWebServerRequest *request)
-{
-    AsyncWebServerResponse *response = request->beginResponse(SPIFFS, "/index.html", "text/html", false, processor);
-    response->addHeader("Cache-Control", "public,max-age=0"); //
-    request->send(response);
-}
-
-void notifyClients(String key, String status)
+void Webservice::notifyClients(String key, String status)
 {
     const uint8_t size = JSON_OBJECT_SIZE(3);
     StaticJsonDocument<size> json;
@@ -100,17 +125,17 @@ void notifyClients(String key, String status)
     {
         json["status"] = status.c_str();
     }
-    
+
     char buffer[size + 10];
     size_t len = serializeJson(json, buffer);
     ESP_LOGI(TAG, "json: %s", buffer);
-    if(ws.count() > 0)
+    if (ws.count() > 0)
     {
         ws.textAll(buffer, len);
     }
 }
 
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+void Webservice::handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
 {
     AwsFrameInfo *info = (AwsFrameInfo *)arg;
     if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
@@ -133,52 +158,25 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
         // Update time
         long utc = json["UTCSeconds"];
         String timeZone = json["Timezone"];
-        timeControl.updateMcuTime(utc, timeZone);
-        config.setTimeZone(timeZone);
+        _updateTime(utc, timeZone);
 
         // Update location
         float latitude = json["Latitude"];
         float longitude = json["Longitude"];
-        config.setGeoLocation(latitude, longitude);
+        _nonVolatileStorage->setGeoLocation(latitude, longitude);
 
         // Update door control
         String doorControl = json["DoorControl"];
-        config.setDoorControl(doorControl);
+        _nonVolatileStorage->setDoorControl(doorControl);
 
         // Update automatic opening and closing time
         String fixOpeningTime = json["AutomaticOpeningTime"];
         String fixClosingTime = json["AutomaticClosingTime"];
-        config.setFixOpeningTime(fixOpeningTime);
-        config.setFixClosingTime(fixClosingTime);
-
-        // Save all parameters
-        config.saveAll();
+        _nonVolatileStorage->setFixOpeningTime(fixOpeningTime);
+        _nonVolatileStorage->setFixClosingTime(fixClosingTime);
 
         notifyClients("feedback", "Data received");
-    }
-}
 
-void onEvent(AsyncWebSocket *server,
-             AsyncWebSocketClient *client,
-             AwsEventType type,
-             void *arg,
-             uint8_t *data,
-             size_t len)
-{
-
-    switch (type)
-    {
-    case WS_EVT_CONNECT:
-        ESP_LOGI(TAG, "WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-        break;
-    case WS_EVT_DISCONNECT:
-        ESP_LOGI(TAG, "WebSocket client #%u disconnected\n", client->id());
-        break;
-    case WS_EVT_DATA:
-        handleWebSocketMessage(arg, data, len);
-        break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-        break;
+        _cbDataReceived();
     }
 }
