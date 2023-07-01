79f70383f8473a814f35ed95b2182cf19e454b0a
Christoph Tack <prog.send@gmail.com>
2023-07-01 13:36:37 +0200

    Adding alarm2, making time zone agnostics

--- firmware/chickenguard/lib/DS1337/DS1337.cpp
+++ firmware/chickenguard/lib/DS1337/DS1337.cpp
@@ -4,6 +4,9 @@
  * @brief Library for DS1337 and PT7C4337 RTC ICs
  * @version 0.1
  * @date 2023-06-18
+ * @note  This library is timezone agnostic.  It is up to the user to convert to and from UTC.  
+ *       I suggest using UTC time for the RTC, so that the RTC can be used in different timezones and doesn't need to be
+ *      updated when the timezone or daylight savings time changes.
  *
  * @details
  * * [PT7C4337](https://www.diodes.com/assets/Datasheets/PT7C4337.pdf)
@@ -15,42 +18,7 @@
 #include "DS1337.h"
 #include "esp32-hal-log.h"
 #include "../../include/bit_manipulation.h"
-static const char* TAG = "DS1337";
-/**
- * @brief Get the Time object as UTC
- * https://stackoverflow.com/questions/530519/stdmktime-and-timezone-info#5157134
- * @param t
- * @return time_t
- */
-static time_t timegm(struct tm *t)
-{
-    long year;
-    time_t result;
-    const int MONTHSPERYEAR = 12; /* months per calendar year */
-    static const int cumdays[MONTHSPERYEAR] =
-        {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
-
-    /*@ +matchanyintegral @*/
-    year = 1900 + t->tm_year + t->tm_mon / MONTHSPERYEAR;
-    result = (year - 1970) * 365 + cumdays[t->tm_mon % MONTHSPERYEAR];
-    result += (year - 1968) / 4;
-    result -= (year - 1900) / 100;
-    result += (year - 1600) / 400;
-    if ((year % 4) == 0 && ((year % 100) != 0 || (year % 400) == 0) &&
-        (t->tm_mon % MONTHSPERYEAR) < 2)
-        result--;
-    result += t->tm_mday - 1;
-    result *= 24;
-    result += t->tm_hour;
-    result *= 60;
-    result += t->tm_min;
-    result *= 60;
-    result += t->tm_sec;
-    if (t->tm_isdst == 1)
-        result -= 3600;
-    /*@ -matchanyintegral @*/
-    return (result);
-}
+static const char *TAG = "DS1337";
 
 uint8_t decode_bcd(uint8_t bcd)
 {
@@ -62,8 +30,10 @@ uint8_t encode_bcd(uint8_t hex)
     return (((hex / 10) & 0x0F) << 4) + (hex % 10);
 }
 
-DS1337::DS1337(int8_t (*readBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, uint8_t *data), bool (*writeBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, const uint8_t *data)) : _readBytes(readBytes),
-                                                                                                                                                                                              _writeBytes(writeBytes)
+DS1337::DS1337(
+    int8_t (*readBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, uint8_t *data),
+    bool (*writeBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, const uint8_t *data)) : _readBytes(readBytes),
+                                                                                               _writeBytes(writeBytes)
 {
 }
 
@@ -76,39 +46,16 @@ uint8_t DS1337::getI2cAddress()
     return I2C_ADDRESS;
 }
 
-
-/**
- * @brief Get the time as UTC seconds since 1970-01-01
- * 
- * @param t 
- * @return true 
- * @return false 
- */
-bool DS1337::getTime(time_t *t)
-{
-    tm timeinfo;
-    if (!getTime(&timeinfo))
-    {
-        return false;
-    }
-    *t = timegm(&timeinfo);
-    return true;
-}
-
 bool DS1337::getTime(tm *timeinfo)
 {
     uint8_t time_data[7];
 
-    if(!isTimeValid())
+    if (!isTimeValid())
     {
         ESP_LOGE(TAG, "Time is not valid");
         return false;
     }
-    if (_readBytes(I2C_ADDRESS, (uint8_t)Register::SECONDS, sizeof(time_data), time_data) != sizeof(time_data))
-    {
-        ESP_LOGE(TAG, "Error reading time");
-        return false;
-    }
+    assert(_readBytes(I2C_ADDRESS, (uint8_t)Register::SECONDS, sizeof(time_data), time_data) == sizeof(time_data));
     timeinfo->tm_sec = decode_bcd(time_data[0]);
     timeinfo->tm_min = decode_bcd(time_data[1]);
     timeinfo->tm_hour = decode_bcd(time_data[2]);
@@ -116,16 +63,11 @@ bool DS1337::getTime(tm *timeinfo)
     timeinfo->tm_mday = decode_bcd(time_data[4]);
     timeinfo->tm_mon = decode_bcd(time_data[5]);
     // 21st century, so adding 100 to the year
-    timeinfo->tm_year = decode_bcd(time_data[6])+100;
-    ESP_LOGI(TAG, "The current UTC date/time is: %s", asctime(timeinfo));
+    timeinfo->tm_year = decode_bcd(time_data[6]) + 100;
+    ESP_LOGI(TAG, "The current RTC date/time is: %s", asctime(timeinfo));
     return true;
 }
 
-bool DS1337::setTime(time_t time)
-{
-    tm *timeinfo = gmtime(&time);
-    return setTime(timeinfo);
-}
 
 bool DS1337::setTime(tm *timeinfo)
 {
@@ -141,46 +83,52 @@ bool DS1337::setTime(tm *timeinfo)
     time_data[5] = encode_bcd(timeinfo->tm_mon);
     // timeinfo->tm_year is years since 1900, needs to be converted to 0-99
     time_data[6] = encode_bcd(timeinfo->tm_year % 100);
-    if (!_writeBytes(I2C_ADDRESS, (uint8_t)Register::SECONDS, sizeof(time_data), time_data))
-    {
-        ESP_LOGE(TAG, "Error writing time");
-        return false;
-    }
-    
+    assert(_writeBytes(I2C_ADDRESS, (uint8_t)Register::SECONDS, sizeof(time_data), time_data));
+
     return (setRegisterBit(Register::CONTROL, Control_nETIME, false) && // Start the clock
-            setRegisterBit(Register::STATUS, Status_OSF, false));   // Clear the oscillator stop flag, indicating that the time is valid again.
+            setRegisterBit(Register::STATUS, Status_OSF, false));       // Clear the oscillator stop flag, indicating that the time is valid again.
 }
 
-bool DS1337::setDailyAlarm1(uint8_t hour, uint8_t minute, uint8_t second)
+bool DS1337::setDailyAlarm(AlarmType alarm, tm *timeinfo)
 {
-    uint8_t alarm_data[3];
+    uint8_t alarm_data[2];
 
-    alarm_data[0] = encode_bcd(second);
-    alarm_data[1] = encode_bcd(minute);
-    alarm_data[2] = encode_bcd(hour);
-    if (!_writeBytes(I2C_ADDRESS, (uint8_t)Register::ALARM1_SECONDS, sizeof(alarm_data), alarm_data))
+    ESP_LOGI(TAG, "Setting RTC alarm %d to %02d:%02d", alarm, timeinfo->tm_hour, timeinfo->tm_min);
+
+    alarm_data[0] = encode_bcd(timeinfo->tm_min);
+    alarm_data[1] = encode_bcd(timeinfo->tm_hour);
+
+    switch (alarm)
     {
-        ESP_LOGE(TAG, "Error writing alarm");
-        return false;
+    case AlarmType::Alarm1:
+        assert(_writeBytes(I2C_ADDRESS, (uint8_t)Register::ALARM1_MINUTES, sizeof(alarm_data), alarm_data));
+        // Set alarm frequency to once per day
+        assert(setRegisterBit(Register::ALARM1_SECONDS, Alarm1Seconds_A1M1, false));
+        assert(setRegisterBit(Register::ALARM1_MINUTES, Alarm1Minutes_A1M2, false));
+        assert(setRegisterBit(Register::ALARM1_HOURS, Alarm1Hours_A1M3, false));
+        assert(setRegisterBit(Register::ALARM1_DAY_DATE, Alarm1DayDate_A1M4, true));
+        // Enable alarm
+        assert(setRegisterBit(Register::CONTROL, Control_A1IE, true));
+        break;
+    case AlarmType::Alarm2:
+        assert(_writeBytes(I2C_ADDRESS, (uint8_t)Register::ALARM2_MINUTES, sizeof(alarm_data), alarm_data));
+        // Set alarm frequency to once per day
+        assert(setRegisterBit(Register::ALARM2_MINUTES, Alarm2Minutes_A2M2, false));
+        assert(setRegisterBit(Register::ALARM2_HOURS, Alarm2Hours_A2M3, false));
+        assert(setRegisterBit(Register::ALARM2_DAY_DATE, Alarm2DayDate_A2M4, true));
+        // Enable alarm
+        assert(setRegisterBit(Register::CONTROL, Control_A2IE, true));
+        break;
+    default:
+        assert(false);
     }
-    // Set alarm frequency to once per day
-    setRegisterBit(Register::ALARM1_SECONDS, Alarm1Seconds_A1M1, false);
-    setRegisterBit(Register::ALARM1_MINUTES, Alarm1Minutes_A1M2, false);
-    setRegisterBit(Register::ALARM1_HOURS, Alarm1Hours_A1M3, false);
-    setRegisterBit(Register::ALARM1_DAY_DATE, Alarm1DayDate_A1M4, true);
-    // Enable alarm
-    setRegisterBit(Register::CONTROL, Control_A1IE, true);
     return true;
 }
 
 bool DS1337::setRegisterBit(Register reg, uint8_t bit, bool value)
 {
     uint8_t reg_data;
-    if (_readBytes(I2C_ADDRESS, (uint8_t)reg, sizeof(reg_data), &reg_data) != sizeof(reg_data))
-    {
-        ESP_LOGE(TAG, "Error reading register");
-        return false;
-    }
+    assert(_readBytes(I2C_ADDRESS, (uint8_t)reg, sizeof(reg_data), &reg_data) == sizeof(reg_data));
     bitWrite(reg_data, bit, value);
     return _writeBytes(I2C_ADDRESS, (uint8_t)reg, sizeof(reg_data), &reg_data);
 }
@@ -190,34 +138,55 @@ bool DS1337::enableSquareWave(bool isEnabled)
     return setRegisterBit(Register::CONTROL, Control_INTCN, !isEnabled);
 }
 
-bool DS1337::acknowledgeAlarm1()
+bool DS1337::acknowledgeAlarm(AlarmType alarm)
 {
-    return setRegisterBit(Register::STATUS, Status_A1F, false);
+    switch (alarm)
+    {
+    case AlarmType::Alarm1:
+        return setRegisterBit(Register::STATUS, Status_A1F, false);
+    case AlarmType::Alarm2:
+        return setRegisterBit(Register::STATUS, Status_A2F, false);
+    default:
+        assert(false);
+    }
 }
 
-bool DS1337::isAlarm1Triggered()
+bool DS1337::isAlarmTriggered(AlarmType alarm)
 {
-    uint8_t status_data;
-    if (_readBytes(I2C_ADDRESS, (uint8_t)Register::STATUS, sizeof(status_data), &status_data) != sizeof(status_data))
+    uint8_t status;
+    assert(readStatusRegister(&status));
+    switch (alarm)
     {
-        ESP_LOGE(TAG, "Error reading status register");
-        return false;
+    case AlarmType::Alarm1:
+        return bitRead(status, Status_A1F);
+    case AlarmType::Alarm2:
+        return bitRead(status, Status_A2F);
+    default:
+        assert(false);
     }
-    return bitRead(status_data, Status_A1F);
 }
 
 bool DS1337::isTimeValid()
 {
-    uint8_t time_data[1];
-    if (_readBytes(I2C_ADDRESS, (uint8_t)Register::STATUS, 1, time_data) != sizeof(time_data))
+    uint8_t status;
+    assert(readStatusRegister(&status));
+    return !bitRead(status, Status_OSF);
+}
+
+bool DS1337::readStatusRegister(uint8_t *status)
+{
+    return _readBytes(I2C_ADDRESS, (uint8_t)Register::STATUS, sizeof(*status), status) == sizeof(*status);
+}
+
+bool DS1337::disableAlarm(AlarmType alarm)
+{
+    switch (alarm)
     {
-        ESP_LOGE(TAG, "Error reading status register");
+    case AlarmType::Alarm1:
+        return setRegisterBit(Register::CONTROL, Control_A1IE, false);
+    case AlarmType::Alarm2:
+        return setRegisterBit(Register::CONTROL, Control_A2IE, false);
+    default:
         return false;
     }
-    if (bitRead(time_data[0], Status_OSF))
-    {
-        ESP_LOGE(TAG, "Oscillator stopped");
-        return false;
-    }
-    return true;
 }
--- firmware/chickenguard/lib/DS1337/DS1337.h
+++ firmware/chickenguard/lib/DS1337/DS1337.h
@@ -6,20 +6,23 @@
 class DS1337
 {
 public:
+    enum class AlarmType
+    {
+        Alarm1,
+        Alarm2
+    };
     DS1337(int8_t (*readBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, uint8_t *data),
            bool (*writeBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, const uint8_t *data));
     ~DS1337();
-    bool getTime(time_t *t);
+    bool isTimeValid();
     bool getTime(tm *timeinfo);
-    bool setTime(time_t time);
     bool setTime(tm *timeinfo);
     uint8_t getI2cAddress();
     bool enableSquareWave(bool isEnabled);
-    bool setDailyAlarm1(uint8_t hour, uint8_t minute, uint8_t second = 0);
-    bool acknowledgeAlarm1();
-    bool isAlarm1Triggered();
-    bool isTimeValid();
-
+    bool setDailyAlarm(AlarmType alarm, tm *timeinfo);
+    bool isAlarmTriggered(AlarmType alarm);
+    bool acknowledgeAlarm(AlarmType alarm);
+    bool disableAlarm(AlarmType alarm);
 private:
     // Register addresses
     enum class Register
@@ -29,6 +32,9 @@ private:
         ALARM1_MINUTES = 0x08,
         ALARM1_HOURS = 0x09,
         ALARM1_DAY_DATE = 0x0A,
+        ALARM2_MINUTES = 0x0B,
+        ALARM2_HOURS = 0x0C,
+        ALARM2_DAY_DATE = 0x0D,
         CONTROL = 0x0E,
         STATUS = 0x0F
     };
@@ -50,19 +56,34 @@ private:
     {
         Alarm1DayDate_A1M4 = 7
     };
+    enum Alarm2MinutesBits
+    {
+        Alarm2Minutes_A2M2 = 7
+    };
+    enum Alarm2HoursBits
+    {
+        Alarm2Hours_A2M3 = 7
+    };
+    enum Alarm2DayDateBits
+    {
+        Alarm2DayDate_A2M4 = 7
+    };
     enum ControlBits
     {
-        Control_nETIME = 7,
-        Control_INTCN = 2,
-        Control_A1IE = 0
+        Control_nETIME = 7, //!< Enable oscillator
+        Control_INTCN = 2,  //!< Interrupt control      
+        Control_A2IE = 1,   //!< Alarm 2 interrupt enable
+        Control_A1IE = 0    //!< Alarm 1 interrupt enable
     };
     enum StatusBits
     {
-        Status_OSF = 7,
-        Status_A1F = 0
+        Status_OSF = 7, //!< Oscillator stop flag
+        Status_A2F = 1, //!< Alarm 2 flag
+        Status_A1F = 0  //!< Alarm 1 flag
     };
     uint8_t const I2C_ADDRESS = 0x68;
     int8_t (*_readBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, uint8_t *data);
     bool (*_writeBytes)(uint8_t i2c_address, uint8_t reg, uint8_t size, const uint8_t *data);
     bool setRegisterBit(Register reg, uint8_t bit, bool value);
+    bool readStatusRegister(uint8_t *status);
 };
